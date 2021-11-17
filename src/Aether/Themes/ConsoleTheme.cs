4b8b71048f7af8cf46a4bcaa0f8a91dc68c5b9c0
Cory Nelson <phrosty@gmail.com>
2021-11-16 17:20:10 -0800

    Add ConsoleTheme (#32)

---
+++ src/Aether/Themes/ConsoleTheme.cs
@@ -0,0 +1,13 @@
+﻿using Aether.Devices.Sensors;
+
+namespace Aether.Themes
+{
+    internal static class ConsoleTheme
+    {
+        public static IDisposable Run(IObservable<Measurement> source) =>
+            source.Subscribe(static (Measurement measurement) =>
+            {
+                Console.WriteLine($"[{DateTime.Now:t}] {measurement.Measure}: {measurement}");
+            });
+    }
+}
